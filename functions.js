c251214b0da98f497a19a9f4afb3160754c2c9d1
Kaan Ibrahim <kaanibrahim1999@gmail.com>
2025-07-11 16:53:54 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,47 @@
+ //3
+ function A3(){
+       document.getElementById("A3").play();
+}
+function Bb3(){
+    document.getElementById("Bb3").play();
+}
+function B3(){
+    document.getElementById("B3").play();
+}
+//4
+function C4(){
+    document.getElementById("C4").play();
+}
+function Db4(){
+    document.getElementById("Db4").play();
+}
+function D4(){
+    document.getElementById("D4").play();
+}
+function Eb4(){
+    document.getElementById("Eb4").play();
+}
+function E4(){
+    document.getElementById("E4").play();
+}
+function F4(){
+    document.getElementById("F4").play();
+}
+function Gb4(){
+    document.getElementById("Gb4").play();
+}
+function G4(){
+    document.getElementById("G4").play();
+}
+function Ab4(){
+    document.getElementById("Ab4").play();
+}
+function A4(){
+    document.getElementById("A4").play();
+}
+function Bb4(){
+    document.getElementById("Bb4").play();
+}
+function B4(){
+    document.getElementById("B4").play();
+}
